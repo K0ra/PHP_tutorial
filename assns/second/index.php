86d8e394e454dd9b4d971aefd18d65d66411b2dc
Korlan <kora.argynova@gmail.com>
2020-11-06 14:13:44 +0600

    PHP tutorial from Coursera initial commit

---
+++ assns/second/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Korlan Argynova PHP</title>
+</head>
+
+<body>
+<h1>Korlan Argynova PHP</h1>
+<pre>ASCII ART:
+
+    **    **
+    **   **
+    **  **
+    ** **
+    **  **
+    **   **
+    **    **
+</pre>
+<?php
+echo 'The SHA256 hash of "Korlan Argynova" is ', hash('sha256', 'Korlan Argynova');
+?>
+</br>
+<a href="check.php">Click here to see an error setting</a>
+</br>
+<a href="fail.php">Click here to cause a traceback</a>
+</body>
